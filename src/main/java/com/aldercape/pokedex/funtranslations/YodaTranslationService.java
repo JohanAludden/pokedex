0211acc4080ac3503327af462cb08a30d9811319
Johan Aludden <jaludden@intuit.com>
2021-12-19 21:50:45 +0000

    Implemented the remote call for yoda transalations

--- src/main/java/com/aldercape/pokedex/funtranslations/YodaTranslationService.java
+++ src/main/java/com/aldercape/pokedex/funtranslations/YodaTranslationService.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 
 @Service
@@ -24,19 +26,18 @@ public class YodaTranslationService implements TranslationService {
     @Override
     public String translate(String text) {
         try {
-            var json = executeHttpPostRequest("https://api.funtranslations.com/translate/yoda.json");
+            var json = executeHttpPostRequest("https://api.funtranslations.com/translate/yoda", text);
             var parsedValue = mapper.readValue(json, new TypeReference<Map<String, Object>>() {});
             var result = (String)((Map<String, Object>)parsedValue.get("contents")).get("translated");
-            System.out.println(result.getClass() + " " + result );
             return result;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String executeHttpPostRequest(String url) {
-        HttpEntity<String> request = new HttpEntity<>("");
-        return client.postForObject(url, request, String.class);
+    public String executeHttpPostRequest(String url, String text) {
+        var fullUrl = UriComponentsBuilder.fromHttpUrl(url).queryParam("text", text).encode().toUriString();
+        return client.getForObject(fullUrl, String.class);
     }
 
 }
--- src/test/java/com/aldercape/pokedex/funtranslations/YodaTranslationServiceTest.java
+++ src/test/java/com/aldercape/pokedex/funtranslations/YodaTranslationServiceTest.java
@@ -1,10 +1,10 @@
 package com.aldercape.pokedex.funtranslations;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,22 +15,33 @@ public class YodaTranslationServiceTest {
     public void testTranslates() {
         var client = mock(RestTemplate.class);
         when(
-                client.postForObject(
-                        eq("https://api.funtranslations.com/translate/yoda.json"),
-                        any(),
+                client.getForObject(
+                        eq("https://api.funtranslations.com/translate/yoda?text=Input"),
                         eq(String.class)))
-                .thenReturn("{\n" +
-                        "  \"success\": {\n" +
-                        "    \"total\": 1\n" +
-                        "  },\n" +
-                        "  \"contents\": {\n" +
-                        "    \"translation\": \"yoda\",\n" +
-                        "    \"text\": \"Input.\",\n" +
-                        "    \"translated\": \"Translated\"\n" +
-                        "  }\n" +
-                        "}");
+                .thenReturn(response);
         var service = new YodaTranslationService(client);
-        var result = service.translate("To be translated");
+        var result = service.translate("Input");
         assertEquals("Translated", result);
     }
+
+    @Test
+    @Disabled
+    // This could be disabled if we want to manually test the connection to the remote api.
+    public void testTranslateWithRemoteRequest() {
+        var service = new YodaTranslationService(new RestTemplate());
+        var result = service.translate("This is a test");
+
+        assertEquals("This%20is%20a%20test", result);
+    }
+
+    private String response = "{\n" +
+            "  \"success\": {\n" +
+            "    \"total\": 1\n" +
+            "  },\n" +
+            "  \"contents\": {\n" +
+            "    \"translation\": \"yoda\",\n" +
+            "    \"text\": \"Input.\",\n" +
+            "    \"translated\": \"Translated\"\n" +
+            "  }\n" +
+            "}";
 }
