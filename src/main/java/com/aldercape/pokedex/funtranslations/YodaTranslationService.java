0b6066e0199470de95832fdfca2b75e4a318c634
Johan Aludden <jaludden@intuit.com>
2021-12-19 21:13:31 +0000

    Added rest template in yoda service

--- src/main/java/com/aldercape/pokedex/funtranslations/YodaTranslationService.java
+++ src/main/java/com/aldercape/pokedex/funtranslations/YodaTranslationService.java
@@ -1,6 +1,8 @@
 package com.aldercape.pokedex.funtranslations;
 
 import com.aldercape.pokedex.TranslationService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,14 +11,21 @@ import java.io.InputStream;
 @Service
 public class YodaTranslationService implements TranslationService {
 
+    private final RestTemplate client;
+
+    public YodaTranslationService(RestTemplate client) {
+        this.client = client;
+    }
+
     @Override
     public String translate(String text) {
         executeHttpPostRequest("https://api.funtranslations.com/translate/yoda.json");
         return "Translated";
     }
 
-    public InputStream executeHttpPostRequest(String url) {
-        return null;
+    public String executeHttpPostRequest(String url) {
+        HttpEntity<String> request = new HttpEntity<>("");
+        return client.postForObject(url, request, String.class);
     }
 
 }
--- src/test/java/com/aldercape/pokedex/funtranslations/YodaTranslationServiceTest.java
+++ src/test/java/com/aldercape/pokedex/funtranslations/YodaTranslationServiceTest.java
@@ -1,26 +1,36 @@
 package com.aldercape.pokedex.funtranslations;
 
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class YodaTranslationServiceTest {
 
-    private String calledUrl;
-
     @Test
     public void testTranslates() {
-        var service = new YodaTranslationService(){
-            public InputStream executeHttpPostRequest(String url) {
-                calledUrl = url;
-                return new ByteArrayInputStream("Translated".getBytes());
-            }
-        };
+        var client = mock(RestTemplate.class);
+        when(
+                client.postForObject(
+                        eq("https://api.funtranslations.com/translate/yoda.json"),
+                        any(),
+                        eq(String.class)))
+                .thenReturn("{\n" +
+                        "  \"success\": {\n" +
+                        "    \"total\": 1\n" +
+                        "  },\n" +
+                        "  \"contents\": {\n" +
+                        "    \"translation\": \"yoda\",\n" +
+                        "    \"text\": \"Input.\",\n" +
+                        "    \"translated\": \"Translated\"\n" +
+                        "  }\n" +
+                        "}");
+        var service = new YodaTranslationService(client);
         var result = service.translate("To be translated");
-        assertEquals("https://api.funtranslations.com/translate/yoda.json", calledUrl);
         assertEquals("Translated", result);
     }
 }
