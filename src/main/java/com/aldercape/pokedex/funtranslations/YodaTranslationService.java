a1a92b38a8870521f4ddcde9de14ff7cc3d084ba
Johan Aludden <jaludden@intuit.com>
2021-12-19 21:19:00 +0000

    Using the value from the remote call in translations

--- src/main/java/com/aldercape/pokedex/funtranslations/YodaTranslationService.java
+++ src/main/java/com/aldercape/pokedex/funtranslations/YodaTranslationService.java
@@ -1,15 +1,19 @@
 package com.aldercape.pokedex.funtranslations;
 
 import com.aldercape.pokedex.TranslationService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.InputStream;
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class YodaTranslationService implements TranslationService {
+
+    private ObjectMapper mapper = new ObjectMapper();
 
     private final RestTemplate client;
 
@@ -19,8 +23,15 @@ public class YodaTranslationService implements TranslationService {
 
     @Override
     public String translate(String text) {
-        executeHttpPostRequest("https://api.funtranslations.com/translate/yoda.json");
-        return "Translated";
+        try {
+            var json = executeHttpPostRequest("https://api.funtranslations.com/translate/yoda.json");
+            var parsedValue = mapper.readValue(json, new TypeReference<Map<String, Object>>() {});
+            var result = (String)((Map<String, Object>)parsedValue.get("contents")).get("translated");
+            System.out.println(result.getClass() + " " + result );
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String executeHttpPostRequest(String url) {
